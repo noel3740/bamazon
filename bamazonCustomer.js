285dbc65c2b37ed2dca1b55202e344304828e856
Noel Nevarez <noel3740@gmail.com>
2019-01-02 10:30:24 -0600

    Adding logic to buy an item.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -26,9 +26,13 @@ connection.connect(function (err) {
 
 //Function that starts the application
 function startApp() {
-    console.log("Started the application!");
+    console.log();
+    console.log("=====================");
+    console.log("WELCOME TO BAMAZON!!!");
+    console.log("=====================");
 
-    endApp();
+    //Ask user which item they wish to buy
+    buyAnItem();
 }
 
 //Function that ends the application
@@ -36,4 +40,86 @@ function endApp() {
     if (connection) {
         connection.end();
     }
+}
+
+//Function that gets products from the bamazon database and displays them in the console.
+//Then prompts the user to select an item to buy and how many.
+function buyAnItem() {
+    //Get all products where the stock quantity is greater than 0
+    connection.query("Select * From products Where stock_quantity > 0",
+        function (err, res) {
+            if (err) throw err;
+
+            const itemsForSale = [];
+
+            //Display items for sale
+            console.log();
+            console.log("=======Current Items For Sale=======");
+            res.forEach((item) => {
+                itemsForSale.push({
+                    name: item.product_name,
+                    value: item.item_id
+                });
+
+                console.log(`${item.product_name} (Item ID ${item.item_id}): $${item.price}`);
+            });
+            console.log("====================================");
+            console.log();
+
+            //Prompt user to select an item to buy
+            inquirer.prompt([
+                {
+                    type: "rawlist",
+                    name: "itemToBuy",
+                    message: "Select an item to buy:",
+                    choices: itemsForSale
+                },
+                {
+                    name: "quantityToBuy",
+                    message: "How many of the item would you like to buy?",
+                    validate: function (quantityToBuy) {
+                        if (isNaN(quantityToBuy) || parseInt(quantityToBuy) <= 0) {
+                            return "Please enter a valid positive number";
+                        } else {
+                            return true;
+                        }
+                    }
+                }
+            ]).then(answer => {
+
+                var itemToBuy = res.find((item) => item.item_id == answer.itemToBuy);
+
+                //If there is enough quantity of the item the fulfill the user's order
+                if (parseInt(itemToBuy.stock_quantity) >= parseInt(answer.quantityToBuy)) {
+                    //Fulfill item order
+                    fulfillItemOrder(itemToBuy, answer.quantityToBuy);
+                }
+                //If there is not enough quantity of the item then display a message letting the user know
+                else {
+                    console.log("Insufficient quantity!");
+                    //End the application
+                    endApp();
+                }
+            });
+        });
+}
+
+//Function that fulfills the customers order to purchase an item
+function fulfillItemOrder(itemToBuy, quantityToBuy) {
+    //Update the products table to reflect remaining quantity
+    connection.query("Update products Set stock_quantity = (stock_quantity - ?) Where item_id = ?",
+        [quantityToBuy, itemToBuy.item_id],
+        function (err, res) {
+            if (err) throw err;
+            //Show the user the total cost of their purchase
+            var totalCost = parseFloat(itemToBuy.price) * parseInt(quantityToBuy);
+            console.log(`Total Cost of Purchase: $${totalCost}`);
+
+            //End the application
+            endApp();
+        });
+
+
+
+
 }
