e570f0f55d39e2497b1c623c2c02b9c7e16a3f95
Noel Nevarez <noel3740@gmail.com>
2019-01-02 11:43:14 -0600

    Adding product_sales to product table, new departments table.

--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -8,5 +8,13 @@ CREATE TABLE IF NOT EXISTS products (
   department_name VARCHAR(200) NOT NULL,
   price DECIMAL(20, 2) NOT NULL default 0,
   stock_quantity INT NOT NULL default 0,
+  product_sales DECIMAL(20, 2) NOT NULL default 0,
   PRIMARY KEY (item_id)
+);
+
+CREATE TABLE IF NOT EXISTS departments (
+  department_id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(200) NOT NULL,
+  over_head_costs DECIMAL(20, 2) NOT NULL default 0,
+  PRIMARY KEY (department_id)
 );
