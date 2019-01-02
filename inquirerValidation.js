b29689a01addd547a14ce2cefd8775124ab8c1d8
Noel Nevarez <noel3740@gmail.com>
2019-01-02 14:32:15 -0600

    Adding validator js to export.

---
+++ inquirerValidation.js
@@ -0,0 +1,20 @@
+//Function will validate that a value entered in inquirer
+function validateValue(value) {
+    return (!value || value.trim().length <= 0) ? false : true;
+}
+
+//Function will validate a money value entered in inquirer
+function validatePositiveMoney(value) {
+    return (isNaN(value) || parseFloat(value) <= 0) ? false : true;
+}
+
+//Function will validate an integer
+function validatePositiveInt(value) {
+    return (isNaN(value) || parseInt(value) < 0) ? false : true;
+}
+
+//Export the functions
+module.exports.validateValue = validateValue;
+module.exports.validatePositiveMoney = validatePositiveMoney;
+module.exports.validatePositiveInt = validatePositiveInt;
+
